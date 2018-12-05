bb65c824de1e5b8dbb34bccb8eaf85a7a10d8688
anoter programing <anoterprograming@meta.com>
2018-12-04 21:30:32 -0300

    mega funcionalidade

--- src/java/CalculadoraServlet.java
+++ src/java/CalculadoraServlet.java
@@ -6,11 +6,17 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import parcels.Operador;
+import parcels.Celula;
+import parcels.TipoOperador;
+import parcels.Valor;
 
 /**
  *
@@ -19,43 +25,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/Calculadora"})
 public class CalculadoraServlet extends HttpServlet {  
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {              
+            throws ServletException, IOException {                              
         request.getRequestDispatcher("/WEB-INF/front/calculadora.jsp").forward(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    }        
 
 }
